a01799a0d94d05751f1e56dadbb35dd382b6c38b
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-14 11:19:43 +0600

    api metho get article by ticket

--- app/api_client/ticket.js
+++ app/api_client/ticket.js
@@ -10,7 +10,7 @@ export async function openedTickets(config) {
   })
 }
 
-export async function getTicketByOwned(config) {
+export async function getTicketsByUser(config) {
   return api_request({
     method: "GET",
     query: `Object=TicketObject&Method=TicketSearch&Data={"StateType":"Open","Result":"ARRAY","OwnerIDs":[${config.userID}],"Permission":"rw"}`,
@@ -22,6 +22,14 @@ export async function ticketGet(config) {
   return api_request({
     method: "GET",
     query: `Object=TicketObject&Method=TicketGet&Data={"TicketID":${config.id}}`,
+    basic: config.basic
+  })
+}
+
+export async function getArticlesByTicket(config) {
+  return api_request({
+    method: "GET",
+    query: `Object=TicketObject&Method=ArticleGet&Data={"TicketID":"${config.ticketID}"}`,
     basic: config.basic
   })
 }
