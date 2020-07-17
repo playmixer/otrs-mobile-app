5ad6b81241158763e48b9cf0acdc0294ae0e8ffa
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-17 10:56:30 +0600

    remove console log

--- app/utils/handleRequestError.js
+++ app/utils/handleRequestError.js
@@ -1,7 +1,6 @@
 import { logout, signInError } from '../store/actions/user'
 
 export default (dispatch) => (error) => {
-  // console.log(error)
   if (error?.status === 401) {
     dispatch(logout())
     dispatch(signInError())
