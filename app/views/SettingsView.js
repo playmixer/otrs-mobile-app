9e7dac1e76a09a3c26e08e70b1be447eb7cc2f3d
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-24 16:09:30 +0600

    page settings

--- app/views/SettingsView.js
+++ app/views/SettingsView.js
@@ -1,19 +1,52 @@
 import React from 'react'
 import styled from 'styled-components/native'
+import {connect} from 'react-redux'
+
+import { subscribe, unSubscribe, checkSubsription } from '../firebase/actions'
+import { getToken } from '../notifications/index'
 
 import Layout from '../components/MainLayout'
 import Switch from '../components/Switch'
 import Text from '../components/Text'
 
-function SettingsView(props) {
+function SettingsView({ user, ticket }) {
   const [isSendPush, setIsSendPush] = React.useState(false)
+
+  const handleChangeSubscribe = () => {
+    setIsSendPush(!isSendPush)
+
+    if (!isSendPush) {
+      return subscribe({ userID: user.model.id, userName: user.model.login, tickets: ticket.listByUser.items})
+    }
+
+    if (isSendPush) {
+      return unSubscribe({ userID: user.model.id })
+    }
+  }
+
+  React.useEffect(() => {
+    let cleanupFunction = false
+
+    if (!cleanupFunction) {
+      checkSubsription({ userID: user.model.id })
+        .then(res => {
+          if (res.toJSON()?.expoToken === getToken()) {
+            setIsSendPush(true)
+          } else {
+            setIsSendPush(false)
+          }
+        })
+    }
+
+    return () => cleanupFunction = true
+  }, [])
 
   return (
     <Layout title="Настройки">
       <Option>
-        <Text>Получить Push уведомления</Text>
+        <Text>Получать Push уведомления</Text>
         <Switch
-          onValueChange={() => {setIsSendPush(!isSendPush)}}
+          onValueChange={handleChangeSubscribe}
           value={isSendPush}
         />
       </Option>
@@ -29,4 +62,7 @@ const Option = styled.View`
   height: 40px;
 `
 
-export default SettingsView;
+export default connect(({ user, ticket }) => ({
+  user: user,
+  ticket: ticket,
+}))(SettingsView)
