76134b630e1973ff10244704081052326309653c
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-24 16:06:12 +0600

    notifications register

--- app/notifications/index.js
+++ app/notifications/index.js
@@ -1,14 +1,20 @@
 import * as Permissions from 'expo-permissions'
 import { Notifications } from 'expo'
 import Constants from 'expo-constants';
+import * as navigation from '../utils/navigation'
 
 let TOKEN = ''
+
+export const getToken = () => {
+  return TOKEN
+}
 
 export const register = async () => {
   if (Constants.isDevice) {
     const { status: existingStatus } = await Permissions.getAsync(
       Permissions.NOTIFICATIONS
     );
+
     let finalStatus = existingStatus;
     if (existingStatus !== 'granted') {
       const { status } = await Permissions.askAsync(
@@ -16,19 +22,34 @@ export const register = async () => {
       );
       finalStatus = status;
     }
+
     if (finalStatus !== 'granted') {
       alert('Failed to get push token for push notification!');
       return;
     }
+
     TOKEN = await Notifications.getExpoPushTokenAsync();
-    console.log(finalStatus, TOKEN);
   } else {
     alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.createChannelAndroidAsync('default', {
+      name: 'default',
+      sound: true,
+      priority: 'max',
+      vibrate: [0, 250, 250, 250],
+    });
   }
 }
 
 const _listen = (notification) => {
-  console.log(notification)
+  if (notification.origin === "selected") {
+    navigation.navigate("ArticleListr", {
+      backView: navigation.state.routeName,
+      ticketID: notification.data.ticketID
+    })
+  }
 }
 
 export const addListener = () => {
