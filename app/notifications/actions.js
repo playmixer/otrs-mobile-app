354d95943b091f4dc8219c12b423b055f639080e
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-24 16:02:20 +0600

    notification actions

---
+++ app/notifications/actions.js
@@ -0,0 +1,22 @@
+import { getToken } from './index'
+
+export const sendPushNotification = (notification) => {
+  const token = getToken()
+  const message = {
+    to: token,
+    sound: 'default',
+    title: notification.title,
+    body: notification.message,
+    data: {...notification.data},
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
