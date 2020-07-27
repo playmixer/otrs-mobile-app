ff3d463d075d9a2f00bbc54989c636525bff45f4
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-27 14:00:11 +0600

    icons for tabBars

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,5 +1,9 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator} from 'react-navigation-tabs'
+
+import { FontAwesome, AntDesign, MaterialIcons } from '@expo/vector-icons'
+import * as appColor from '../modules/colors'
 
 import UserTicketsNavigator from './UserTicketsNavigator'
 import TicketNavigator from './TicketsNavigator'
@@ -11,24 +15,32 @@ const AppNavigator = createBottomTabNavigator({
     screen: UserTicketsNavigator,
     navigationOptions: {
       title: "Мои заявки",
+      tabBarIcon: ({ tintColor }) => (<FontAwesome name="user" size={24} color={tintColor} />)
     }
   },
   Tickets: {
     screen: TicketNavigator,
     navigationOptions: {
       title: "Открытые заявки",
+      tabBarIcon: ({ tintColor }) => (<FontAwesome name="list" size={24} color={tintColor} />)
     }
   },
   Settings: {
     screen: SettingsView,
     navigationOptions: {
-      title: "Настройки"
+      title: "Настройки",
+      tabBarIcon: ({ tintColor }) => (<MaterialIcons name="settings" size={24} color={tintColor} />)
     }
   },
 }, {
   initialRouteName: 'UserTickets',
   backBehavior: 'none',
   lasy: false,
+  tabBarOptions: {
+    activeTintColor: appColor.main,
+    inactiveTintColor: "black"
+  }
 })
+
 
 export default createAppContainer(AppNavigator);
