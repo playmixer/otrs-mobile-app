45f7756bb74cd65d4bd79bc11118e06124cda99a
Shkirya.NS <shkirya.ns@oilpc.ru>
2020-07-30 10:17:37 +0600

    init notification reducer

--- app/store/reducers/index.js
+++ app/store/reducers/index.js
@@ -1,7 +1,9 @@
-import userReducer from './user'
-import ticketReducer from './ticket'
-import errorReducer from './error'
+import userReducer from './user';
+import ticketReducer from './ticket';
+import errorReducer from './error';
+import notificationReducer from './notifications';
 
-export const user = userReducer
-export const ticket = ticketReducer
-export const error = errorReducer
+export const user = userReducer;
+export const ticket = ticketReducer;
+export const error = errorReducer;
+export const notification = notificationReducer;
